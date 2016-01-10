4a34a32a89278bc881f2ec7ec78d45dbc428cfaa
ryansgot <ryansgot@gmail.com>
2016-01-09 22:29:36 -0600

    update NewFinderGenerator to have javadoc on class

--- src/main/java/com/forsuredb/annotationprocessor/generator/code/NewFinderGenerator.java
+++ src/main/java/com/forsuredb/annotationprocessor/generator/code/NewFinderGenerator.java
@@ -19,9 +19,11 @@ public class NewFinderGenerator extends JavaSourceGenerator {
 
     private List<ColumnInfo> columnsSortedByName;
     private final ClassName[] parameterClasses;
+    private final TableInfo table;
 
     public NewFinderGenerator(ProcessingEnvironment processingEnv, TableInfo table) {
         super(processingEnv, table.getQualifiedClassName() + "Finder");
+        this.table = table;
         columnsSortedByName = ColumnUtil.columnsSortedByName(table);
         parameterClasses = new ClassName[] {
                 ClassName.get(getResultParameter()),                            // U (the resultParameter)
@@ -31,9 +33,53 @@ public class NewFinderGenerator extends JavaSourceGenerator {
         };
     }
 
+    /**
+     * <p>
+     *   This is an auto-generated class. DO NOT modify it!
+     * </p>
+     * <p>
+     *   Provides methods for creating a query that will query the test_table_3
+     *   table. These methods can be chained to produce just about any query you
+     *   may want, for example:
+     *     <pre>
+     *       {@code
+     *               testTable3().find()
+     *               .byIdBetweenInclusive(23545494583L)
+     *               .andInclusive(23545494583L)
+     *               .byCreatedBefore(new Date())
+     *               .andFinally()
+     *               .get();
+     *       }
+     *     </pre>
+     * </p>
+     * @author <a href="https://github.com/ryansgot/forsuredbcompiler">forsuredbcompiler</a>
+     * @see Resolver
+     */
     @Override
     protected String getCode() {
+        JavadocInfo jd = JavadocInfo.builder()
+                .startParagraph()
+                .addLine("This is an auto-generated class. DO NOT modify it!")
+                .endParagraph()
+                .startParagraph()
+                .addLine("Provides methods for creating a query that will query the $L", table.getTableName())
+                .addLine("table. These methods can be chained to produce just about any query you")
+                .addLine("may want, for example:")
+                .startCode()
+                .addLine("$L().find()", CodeUtil.snakeToCamel(table.getTableName()))
+                .addLine(".byIdBetweenInclusive($L)", CodeUtil.javaExampleOf("long"))
+                .addLine(".andInclusive($L)", CodeUtil.javaExampleOf("long"))
+                .addLine(".byCreatedBefore($L)", CodeUtil.javaExampleOf("java.util.Date"))
+                .addLine(".andFinally()")
+                .addLine(".get();")
+                .endCode()
+                .endParagraph()
+                .addLine(JavadocInfo.AUTHOR_STRING)
+                .addLine("@see Resolver")
+                .addLine()
+                .build();
         TypeSpec.Builder codeBuilder = TypeSpec.classBuilder(getOutputClassName(false))
+                .addJavadoc(jd.stringToFormat(), jd.replacements())
                 .addModifiers(Modifier.PUBLIC)
                 .superclass(ParameterizedTypeName.get(ClassName.get(Finder.class), parameterClasses));
         addConstructor(codeBuilder);
