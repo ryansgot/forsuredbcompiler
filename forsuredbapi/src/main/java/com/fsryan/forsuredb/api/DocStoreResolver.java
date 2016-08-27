4ce355b297ceecec1c3d1d0145a1720226cde464
Ryan Scott <ryansgot@gmail.com>
2016-08-27 01:09:15 -0500

    remove notes

--- forsuredbapi/src/main/java/com/fsryan/forsuredb/api/DocStoreResolver.java
+++ forsuredbapi/src/main/java/com/fsryan/forsuredb/api/DocStoreResolver.java
@@ -1,9 +1,5 @@
 package com.fsryan.forsuredb.api;
 
-// 1. For OrderBy, and Finder, ensure that the Setter class is within type bounds
-// 2. Update forsuredb's sqlitelib to correctly interpret @Index and @Unique annotations
-// 3. Implement the initial serialization as default GSON serialization
-
 public abstract class DocStoreResolver<T, U, R extends RecordContainer, G extends FSDocStoreGetApi<T>, S extends FSDocStoreSaveApi<U, T>, F extends DocStoreFinder<T, U, R, G, S, F, O>, O extends DocStoreOrderBy<T, U, R, G, S, F, O>> extends Resolver<U, R, G, S, F, O> {
     public DocStoreResolver(ForSureInfoFactory<U, R> infoFactory) {
         super(infoFactory);
---
+++ forsuredbapi/src/test/java/com/fsryan/forsuredb/api/adapter/JsonAdapterHelperTest.java
@@ -0,0 +1,7 @@
+package com.fsryan.forsuredb.api.adapter;
+
+/**
+ * Created by ryan on 8/27/16.
+ */
+public class JsonAdapterHelperTest {
+}
