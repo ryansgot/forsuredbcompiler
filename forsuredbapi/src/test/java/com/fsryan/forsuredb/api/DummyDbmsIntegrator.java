f513f9d005f777bd741ee323a1d20157c5c03bfc
ryansgot <ryansgot@gmail.com>
2016-10-19 00:36:25 -0500

    a DbmsIntegrator for tests only

---
+++ forsuredbapi/src/test/java/com/fsryan/forsuredb/api/DummyDbmsIntegrator.java
@@ -0,0 +1,31 @@
+package com.fsryan.forsuredb.api;
+
+import com.fsryan.forsuredb.api.migration.MigrationSet;
+import com.fsryan.forsuredb.api.sqlgeneration.DBMSIntegrator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>
+ *     Since now the operation of the api does not have a default for {@link DBMSIntegrator}, this is necessary.
+ * </p>
+ */
+public class DummyDbmsIntegrator implements DBMSIntegrator {
+
+    @Override
+    public List<String> generateMigrationSql(MigrationSet migrationSet) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String newSingleRowInsertionSql(String tableName, Map<String, String> columnValueMap) {
+        return "";
+    }
+
+    @Override
+    public String unambiguousColumn(String tableName, String columnName) {
+        return tableName + "_" + columnName;
+    }
+}
