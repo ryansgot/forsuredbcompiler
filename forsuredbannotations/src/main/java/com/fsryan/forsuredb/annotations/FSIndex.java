bc64764bad48f27a7c062105d2d53a54dd744799
ryansgot <ryansgot@gmail.com>
2018-09-22 20:48:15 -0700

    deprecate @Index in favor of @FSIndex

--- forsuredbannotations/src/main/java/com/fsryan/forsuredb/annotations/FSIndex.java
+++ forsuredbannotations/src/main/java/com/fsryan/forsuredb/annotations/FSIndex.java
@@ -13,14 +13,14 @@ import java.lang.annotation.Target;
  * <p>Due to the fact that you can create an index that is not unique on some
  * DBMS implementations, there is also a {@link Unique} annotation. Thus, the
  * following are equivalent:
- * Using only {@link Index}:</br>
+ * Using only {@link FSIndex}:</br>
  * <pre><code>   public interface MyTable extends FSGetApi {
  *    {@literal @}FSColumn("my_column")
  *    {@literal @}Index(unique = true)
  *     String uuid(Retriever r)
  * }
  * </code></pre>
- * Using {@link Index} and {@link Unique}:</br>
+ * Using {@link FSIndex} and {@link Unique}:</br>
  * <pre><code>   public interface MyTable extends FSGetApi {
  *    {@literal @}FSColumn("my_column")
  *    {@literal @}Index
@@ -36,18 +36,42 @@ import java.lang.annotation.Target;
  * }
  * </code></pre>
  *
- * @deprecated Due to the fact that you should be using {@link FSIndex}
- * instead. {@link FSIndex} is capable of using composites and
- * sort order to further specify your indices. This will be removed in 1.0.0.
+ * <p>Composite indices are allowed, and they are defined in the same way
+ * composite foreign keys are defined using {@link FSForeignKey#compositeId()}.
+ * All methods marked with {@link FSIndex}, having the same nonempty compositeId
+ * will be part of the same composite index.
+ *
  * @author Ryan Scott
  */
-@Deprecated
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.METHOD)
-public @interface Index {
+public @interface FSIndex {
     /**
-     * <p>Defaults to false. See {@link Index} for more information
+     * <p>Defaults to false. See {@link FSIndex} for more information
      * @return whether this column should also be an index of the table
      */
     boolean unique() default false;
+
+    /**
+     * <p>If this annotation is marking a column as part of a composite index,
+     * Then you should provide a compositeId that is not the empty string.
+     *
+     * <p>Note that if you specify one member of the composite as unique, and
+     * another member of the composite as not unique, the compiler will throw
+     * an error.
+     *
+     * @return the compositeId string for this member of a composite index
+     * @since 0.14.0
+     */
+    String compositeId() default "";
+
+    /**
+     * <p>This is DBMS-dependant, but many DBMS implementations allow you to
+     * specify the sort order of the index. If not specified, the DBMS default
+     * will be used.
+     *
+     * @return the sort order of the index
+     * @since 0.14.0
+     */
+    String sortOrder() default "";
 }
